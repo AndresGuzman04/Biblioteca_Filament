7444b7bf06db1297e57372bce38f40cc3b8614e1
AndresGuzman04 <andr35guzman2001@gmail.com>
2024-11-22 21:01:38 -0600

    Theme modified

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -36,8 +36,14 @@ class AdminPanelProvider extends PanelProvider
             //email verification
             //->emailVerification()
             ->colors([
-                'primary' => Color::Amber,
+                'danger' => Color::Rose,
+                'gray' => Color::Gray,
+                'info' => Color::Blue,
+                'primary' => Color::Indigo,
+                'success' => Color::Emerald,
+                'warning' => Color::Orange,
             ])
+            ->brandName('Biblioteca')
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
             ->pages([
