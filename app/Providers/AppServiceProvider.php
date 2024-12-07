4e54a41f2278f8019bfe0caf0c214fbdcfaff4a9
AndresGuzman04 <andr35guzman2001@gmail.com>
2024-12-07 08:26:47 -0600

    Add Force https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -22,6 +22,9 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Model::unguard();
+        if (config('app.env') === 'production') {
+            URL::forceScheme('https');
+        }
     }
 
 }
